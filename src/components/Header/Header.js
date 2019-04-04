b76ce4ea775719e21766ca2507eb14939c0afe35
Jake <jakeadmire1@gmail.com>
2019-04-04 09:35:04 -0600

    Replace 'header' text with 'TrapperKeeper' & add placeholder logo image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import icon from '../../media/icon-placeholder.png';
 
 export const Header = () => {
   return (
-    <div className="Header">Header</div>
+    <div className="Header">
+      <img alt="" src={icon}/>
+      <h1>TrapperKeeper</h1>
+      <Link to="/new-note">New Note</Link>
+    </div>
   )
 }

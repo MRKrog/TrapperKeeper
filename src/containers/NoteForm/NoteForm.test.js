8537bd7ff77f13229bf3870011cb161531b1c87e
David Cisneros <Dcisneros101@gmail.com>
2019-04-05 11:14:48 -0600

    Start snapshot for noteForm

--- src/containers/NoteForm/NoteForm.test.js
+++ src/containers/NoteForm/NoteForm.test.js
@@ -2,16 +2,29 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { NoteForm } from './NoteForm';
 
+const mockShortid = jest.fn().mockReturnValue('12345789');
+
+let mockNote = { id: mockShortid,
+  title: 'Worf ToDo',
+  list: [
+    { id: mockShortid,
+      text: 'Eat food',
+      isComplete: false
+    },
+  ]
+};
 
 describe('NoteForm', () => {
   let wrapper;
 
   beforeEach(() => {
-    wrapper = shallow(<NoteForm />)
-  })
+    wrapper = shallow(<NoteForm type="existing-note" noteId={mockShortid} {...mockNote}/>)
+  });
 
   it('should match the snapshot', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
 
-  })
+  
 
 })

9cf4c6a51e1cb38283a4cc28f7879b0e6f3cb0eb
jessicalynhansen@gmail.com <jessicalynhansen@gmail.com>
2019-04-05 16:33:31 -0600

    Update fetchData with response.json and update test

--- src/utility/fetchData.js
+++ src/utility/fetchData.js
@@ -1,8 +1,9 @@
 export const fetchData = async (url, body) => {
   const response = await fetch(url, body)
+  const result = await response.json()
   if(!response.ok){
-    throw new Error(response.statusText)
+    throw new Error(result)
   } else {
-    return await response.json()
+    return result
   }
 }

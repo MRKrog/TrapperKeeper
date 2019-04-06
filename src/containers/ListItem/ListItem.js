9588783a45d9eaf2c51a04426de635cb300ba406
Jake <jakeadmire1@gmail.com>
2019-04-06 11:59:10 -0600

    Add 'handleEnter' method within NoteForm.js to allow new listItem's to be appended on 'enter' keypress

--- src/containers/ListItem/ListItem.js
+++ src/containers/ListItem/ListItem.js
@@ -1,27 +1,56 @@
-import React from 'react';
+// import React from 'react';
 
-export const ListItem = (props) => {
+// export const ListItem = (props) => {
 
-  const { id, index, isComplete, text, handleItemChange, handleItemDelete, toggleComplete } = props;
+//   const { id, index, isComplete, text, handleItemChange, handleItemDelete, toggleComplete } = props;
 
-  return (
-    <li key={index}>
-      <label className="container">
-        <input onClick={() => toggleComplete(id)} type="checkbox" className={isComplete ? 'checkbox checked' : 'checkbox not-checked'}/>
-        <span className="checkmark"></span>
-      </label>
-      <input type="text"
-              placeholder="List Item"
-              value={text}
-              name="ListItem"
-              className="list-item-input"
-              onChange={(e) => handleItemChange(e, id, index)}
-              disabled={isComplete ? 'disabled' : null}
-        />
-        <button className="delete-button" onClick={(e) => handleItemDelete(e, index)}>
-          <i className="fas fa-times"></i>
-        </button>
-    </li>
-  )
+//   return (
+//     <li key={index}>
+//       <label className="container">
+//         <input onClick={() => toggleComplete(id)} type="checkbox" className={isComplete ? 'checkbox checked' : 'checkbox not-checked'}/>
+//         <span className="checkmark"></span>
+//       </label>
+//       <input type="text"
+//               placeholder="List Item"
+//               value={text}
+//               name="ListItem"
+//               className="list-item-input"
+//               onChange={(e) => handleItemChange(e, id, index)}
+//               disabled={isComplete ? 'disabled' : null}
+//         />
+//         <button className="delete-button" onClick={(e) => handleItemDelete(e, index)}>
+//           <i className="fas fa-times"></i>
+//         </button>
+//     </li>
+//   )
 
+// }
+
+import React, { Component } from 'react';
+
+export class ListItem extends Component {
+  
+  render() {
+    const { id, index, isComplete, text, handleItemChange, handleItemDelete, toggleComplete } = this.props;
+    return (
+      <li key={index}>
+        <label className="container">
+          <input onClick={() => toggleComplete(id)} type="checkbox" className={isComplete ? 'checkbox checked' : 'checkbox not-checked'}/>
+          <span className="checkmark"></span>
+        </label>
+        <input type="text"
+                placeholder="List Item"
+                value={text}
+                name="ListItem"
+                className="list-item-input"
+                onChange={(e) => handleItemChange(e, id, index)}
+                disabled={isComplete ? 'disabled' : null}
+                id={id}
+          />
+          <button className="delete-button" onClick={(e) => handleItemDelete(e, index)}>
+            <i className="fas fa-times"></i>
+          </button>
+      </li>
+    )
+  }
 }

a88058e2d1fadf909a4e2e5a4f82a4e87ebfe8ab
Jake <jakeadmire1@gmail.com>
2019-04-08 15:47:17 -0600

    Build loadingReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import { notesReducer } from './notesReducer';
 import { errorReducer } from './errorReducer';
+import { loadingReducer } from './loadingReducer';
 
 export const rootReducer = combineReducers({
   allNotes: notesReducer,
-  error: errorReducer
+  error: errorReducer,
+  loading: loadingReducer
 })

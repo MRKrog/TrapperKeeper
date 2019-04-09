3ebf12e793f6f9bf2531594a09e8c58fdb5eabd1
Jake <jakeadmire1@gmail.com>
2019-04-09 15:08:57 -0600

    Remove console log from errorReducer
    Co-authored-by: Jessica Hansen <jessicalynhansen@gmail.com>

--- src/reducers/errorReducer.js
+++ src/reducers/errorReducer.js
@@ -1,7 +1,6 @@
 export const errorReducer = (state = "", action) => {
   switch (action.type) {
     case 'HAS_ERROR':
-    console.log('in error reducer');
       return action.message
     default:
       return state
